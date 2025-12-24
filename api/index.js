76f8a9e51c1b2974cbcae1d4760bd794428f501a
ntwalisonga22-alt <ntwalisonga22@gmail.com>
2025-12-24 13:22:21 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,16 +5,12 @@ export default async function handler(req, res) {
         const news = await kv.get('news_data') || [];
         return res.status(200).json(news);
     }
-
     if (req.method === 'POST') {
         const { password, title, content, image } = req.body;
         if (password !== '1234') return res.status(401).send("Unauthorized");
-
         let current = await kv.get('news_data') || [];
         current.unshift({
-            title, 
-            description: content, 
-            image: image || null,
+            title, description: content, image: image || null,
             date: new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })
         });
         await kv.set('news_data', current.slice(0, 20));
